4b741e99ccfba9eb4e15a47baefbf061a76da80e
Maicon Ferreira <maicon.ferreira@trentim.com>
2020-12-13 02:30:15 -0300

    interface

--- client/eadclient/src/pages/Login/Login.tsx
+++ client/eadclient/src/pages/Login/Login.tsx
@@ -1,12 +1,21 @@
 
 import React from 'react';
-import { DefaultButton } from '@fluentui/react'
-
+import { DefaultButton, TextField, Label } from '@fluentui/react';
+import Logo from '../../assets/logo.png'
+import './styles.css';
+import '../../../src/index.css'
 
 export default function Login() {
     return (
-        <div>
-            <h1>LOGIN</h1>
+        <div className="loginContainer">
+            <div className="loginForm">
+                <img src={Logo} alt="Logo" className="Logo" />
+                <TextField label="E-mail" />
+                <TextField label="Senha" type="password" canRevealPassword />
+                <Label>Ainda não tem uma conta? Cadastre-se <a href="#"> aqui </a> </Label>
+                <Label>Esqueceu sua senha? Recupere <a href="#"> aqui </a> </Label>
+                <DefaultButton text="Login" />
+            </div>
         </div>
     )
 }
